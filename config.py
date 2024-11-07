e6dd66ecfafacf9c37ad0b5792ad2c3523073dcd
official-ztx <wtuhin707@gmail.com>
2024-11-07 10:50:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,9 +80,6 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/AJT.jpg"
-)
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/AJT.jpg"
 )
