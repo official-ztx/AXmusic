8bc99881e0f00dcedbffc7cc9bf100867f5f324e
Zeta-05 <vjimmy276@gmail.com>
2024-11-07 09:21:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/j1E.mp4"
+    "START_IMG_URL", "https://envs.sh/AJT.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/AJT.jpg"
