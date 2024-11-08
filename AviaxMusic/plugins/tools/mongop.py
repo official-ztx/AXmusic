9341b04cabcf65f73d1a2729543220493a93a2e1
Zeta-05 <vjimmy276@gmail.com>
2024-11-08 19:10:16 +0530

    Update and rename mongo.py to mongop.py

--- AviaxMusic/plugins/tools/mongop.py
+++ AviaxMusic/plugins/tools/mongop.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.AXmusic
+db = mongo.AviaxMusic
 
 coupledb = db.couple
 
